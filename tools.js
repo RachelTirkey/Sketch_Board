798d437c652a4f0021586d937f41c21c148d554e
Rachel Tirkey <13rtirkey@gmail.com>
2024-03-14 08:42:04 +0530

    added code for hiding pencil color and eraser thickness

--- tools.js
+++ tools.js
@@ -1,4 +1,6 @@
 let toolsCont = document.querySelector(".tools-cont");
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
 
 let optionsCont = document.querySelector(".options-cont");
 let optionsFlag = true;
@@ -16,10 +18,14 @@ function openTools() {
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
     toolsCont.style.display = "flex";
+
 }
 function closeTools() {
   let iconElem = optionsCont.children[0];
   iconElem.classList.remove("fa-bars");
   iconElem.classList.add("fa-times");
   toolsCont.style.display = "none";
+  
+  pencilToolCont.style.display = "none";
+  eraserToolCont.style.display = "none";
 }
