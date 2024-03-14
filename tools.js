9b88cd970689a0a489209d1b2d8ef83b16472b8b
Rachel Tirkey <13rtirkey@gmail.com>
2024-03-14 08:35:47 +0530

    added code for hiding elements container when close options is clicked

--- tools.js
+++ tools.js
@@ -1,3 +1,5 @@
+let toolsCont = document.querySelector(".tools-cont");
+
 let optionsCont = document.querySelector(".options-cont");
 let optionsFlag = true;
 
@@ -13,9 +15,11 @@ function openTools() {
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
+    toolsCont.style.display = "flex";
 }
 function closeTools() {
   let iconElem = optionsCont.children[0];
   iconElem.classList.remove("fa-bars");
   iconElem.classList.add("fa-times");
+  toolsCont.style.display = "none";
 }
