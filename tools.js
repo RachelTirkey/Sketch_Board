94dee7aa5e217e09e6c726c25bfc69b4d4538726
Rachel Tirkey <13rtirkey@gmail.com>
2024-03-14 09:06:46 +0530

    added code for toggle function in pencil and eraser tools

--- tools.js
+++ tools.js
@@ -1,12 +1,17 @@
 let toolsCont = document.querySelector(".tools-cont");
 let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let pencilFlag = false;
+let eraserFlag = false;
 
 let optionsCont = document.querySelector(".options-cont");
 let optionsFlag = true;
 
-// true -> tools shhow, false -> hide tools
+
 optionsCont.addEventListener("click", (e) => {
+  // true -> tools shhow, false -> hide tools
   optionsFlag = !optionsFlag;
 
   if(optionsFlag) openTools();
@@ -25,7 +30,23 @@ function closeTools() {
   iconElem.classList.remove("fa-bars");
   iconElem.classList.add("fa-times");
   toolsCont.style.display = "none";
-  
+
   pencilToolCont.style.display = "none";
   eraserToolCont.style.display = "none";
 }
+
+pencil.addEventListener("click", (e) => {
+  // true -> show pencil tool, false -> hide pencil tool
+    pencilFlag = !pencilFlag;
+
+    if (pencilFlag) pencilToolCont.style.display = "block";
+    else pencilToolCont.style.display = "none";
+})
+
+eraser.addEventListener("click", (e) => {
+  // true -> show eraser tool, false -> hide eraser tool
+    eraserFlag = !eraserFlag;
+
+    if (eraserFlag) eraserToolCont.style.display = "flex";
+    else eraserToolCont.style.display = "none";
+})
